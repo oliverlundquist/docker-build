4f8e45918a0f856d53908457a09b3ca5fbef8ffb
oliverlundquist <oliver@mystore.no>
2017-08-26 13:38:09 +0200

    updated build scripts

---
+++ build_php.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+make build-php
+make build-php-opcache
+make build-php-xdebug
+make build-php-webgrind
+make build-php-queue-worker
---
+++ deploy_php.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+make deploy-php
+make deploy-php-opcache
+make deploy-php-xdebug
+make deploy-php-webgrind
+make deploy-php-queue-worker
